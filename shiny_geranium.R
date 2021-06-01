45d986f06f198b8fcc98eb1f56cb418e115c9965
enikkel <nikkel.emma@gmail.com>
2021-05-31 17:28:16 -0700

    Downloaded main dataset

--- shiny_geranium.R
+++ shiny_geranium.R
@@ -4,8 +4,14 @@
 library(rgbif)
 library(scrubr)
 library(maps)
+library(dplyr)
 
 myspecies <- c("Geranium lucidum")
-shiny_geranium_gbif <- occ_data(scientificName = myspecies, hasCoordinate = TRUE)
+shiny_geranium_gbif <- occ_data(scientificName = myspecies, hasCoordinate = TRUE, hasGeospatialIssue = FALSE, limit = 40000)
+shiny_geranium_gbif
+   
+Geranium_data <- shiny_geranium_gbif$data[ , c("decimalLongitude", "decimalLatitude", "issues", "individualCount", "occurrenceStatus", 
+                                               "coordinateUncertaintyInMeters", "institutionCode", "references", "basisOfRecord", 
+                                               "year", "month", "day", "eventDate", "geodeticDatum")] 
 
                                 
