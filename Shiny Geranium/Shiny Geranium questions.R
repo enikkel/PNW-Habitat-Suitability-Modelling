95ffd3f7fd76ebe7b9b5568ccaaa5e81737c5a85
enikkel <nikkel.emma@gmail.com>
2021-06-04 16:10:21 -0700

    Questions/things to work on

---
+++ Shiny Geranium/Shiny Geranium questions.R
@@ -0,0 +1,13 @@
+##### Shiny Geranium Stats and Questions #####
+
+table(Geranium_data_train$basisOfRecord)
+# 2043 Human observations
+# 14 Observation
+# 123 Preserved Specimens
+# 1 Unknown
+
+# Keep Preserved Specimens??
+
+table(Geranium_data_test$basisOfRecord)
+# 90 Preserved
+# 7 Unknown
